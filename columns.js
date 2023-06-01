bbca911d95e392084d64a42b080acf95aa28e10f
sodavinchheng-quad <sodavin.chheng@quad.co.jp>
2023-06-01 18:45:06 +0900

    feat: search header column by name

--- columns.js
+++ columns.js
@@ -1,19 +1,19 @@
 const headerRow = 2;
 
 const columns = {
-  'number': 1,            // No
-  'page': 2,              // ページ
-  'type': 3,              // 正常/ 異常
-  'pov': 4,               // 観点
-  'test_case': 5,         // テストケース
-  'operation': 6,         // 操作
-  'input_data': 7,        // テストデータ
-  'item': 8,              // 項目
-  'expected_result': 9,   // 期待する結果
-  'actual_result': 10,    // 結果
-  'ios': 11,              // 判定 (iOS)
-  'android': 12,          // 判定 (android)
-  'pc': 13,               // 判定 (PC)
-  'remark': 14,           // 備考
-  'issue': 15
+  'number': 'No',
+  'page': 'ページ',
+  'type': '正常/ 異常',
+  'pov': '観点',
+  'test_case': 'テストケース',
+  'operation': '操作',
+  'input_data': 'テストデータ',
+  'item': '項目',
+  'expected_result': '期待する結果',
+  'actual_result': '結果',
+  'ios': '判定 (iOS)',
+  'android': '判定 (android)',
+  'pc': '判定 (PC)',
+  'remark': '備考',
+  'issue': 'Issue'
 };
--- main.js
+++ main.js
@@ -1,57 +1,67 @@
-function main() {
-  const activeSheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheets = activeSheet.getSheets();
+const activeSheet = SpreadsheetApp.getActiveSpreadsheet();
+const sheets = activeSheet.getSheets();
 
+function main() {
   for (let sheetNo = 0; sheetNo < sheets.length; sheetNo++) {
     const sheet = sheets[sheetNo];
 
+    function getColumnNo(columnKey) {
+      let colNo = 1;
+      let res = null;
+      while (true) {
+        let columnName = sheet.getRange(headerRow, colNo).getValue();
+
+        if (columnName.length === 0) break;
+
+        if (columnName === columns[columnKey]) {
+          res = colNo;
+          break;
+        }
+
+        colNo++;
+      }
+
+      return res;
+    }
+
     let rowNo = headerRow + 1;
     while (true) {
-      const number = sheet.getRange(rowNo, columns["number"]).getValue();
-      const testCase = sheet.getRange(rowNo, columns["test_case"]).getValue();
+      const number = sheet.getRange(rowNo, getColumnNo('number')).getValue();
+      const testCase = sheet.getRange(rowNo, getColumnNo('test_case')).getValue();
 
       if (number.length === 0 || testCase.length === 0) break;
 
-      if (sheet.getRange(rowNo, columns["issue"]).getValue().length > 0) {
+      if (sheet.getRange(rowNo, getColumnNo('issue')).getValue().length > 0) {
         rowNo++;
         continue;
       }
 
-      const ios = sheet.getRange(rowNo, columns["ios"]).getValue();
-      const android = sheet.getRange(rowNo, columns["android"]).getValue();
-      const pc = sheet.getRange(rowNo, columns["pc"]).getValue();
+      const ios = sheet.getRange(rowNo, getColumnNo('ios')).getValue();
+      const android = sheet.getRange(rowNo, getColumnNo('android')).getValue();
+      const pc = sheet.getRange(rowNo, getColumnNo('pc')).getValue();
 
-      if (ios === "NG" || android === "NG" || pc === "NG") {
+      if (ios === 'NG' || android === 'NG' || pc === 'NG') {
         const sheetLink = `https://docs.google.com/spreadsheets/d/${activeSheet.getId()}/edit#gid=${sheet.getSheetId()}&range=${rowNo}:${rowNo}`;
 
         const title = testCase;
         const body = bodyTemplate
-          .replace("{SHEET_LINK}", sheetLink)
-          .replace("{TEST_CASE_NUMBER}", number)
-          .replace("{PROBLEM_BACKGROUND}", testCase)
-          .replace("{SCREEN_OR_API_ID}", sheet.getName())
-          .replace(
-            "{PROBLEM_DETAIL}",
-            sheet.getRange(rowNo, columns["actual_result"]).getValue()
-          )
-          .replace(
-            "{HOW_TO_REPRODUCE}",
-            sheet.getRange(rowNo, columns["operation"]).getValue()
-          )
-          .replace(
-            "{CORRECT_BEHAVIOR}",
-            sheet.getRange(rowNo, columns["expected_result"]).getValue()
-          );
+          .replace('{SHEET_LINK}', sheetLink)
+          .replace('{TEST_CASE_NUMBER}', number)
+          .replace('{PROBLEM_BACKGROUND}', testCase)
+          .replace('{SCREEN_OR_API_ID}', sheet.getName())
+          .replace('{PROBLEM_DETAIL}', sheet.getRange(rowNo, getColumnNo('actual_result')).getValue())
+          .replace('{HOW_TO_REPRODUCE}', sheet.getRange(rowNo, getColumnNo('operation')).getValue())
+          .replace('{CORRECT_BEHAVIOR}', sheet.getRange(rowNo, getColumnNo('expected_result')).getValue());
 
         const payload = {
           title,
-          body,
+          body
         };
 
         const url = createIssue(payload);
 
-        sheet.getRange(rowNo, columns["issue"]).setValue(url);
-        console.log("New Issue Created: ", url);
+        sheet.getRange(rowNo, getColumnNo('issue')).setValue(url);
+        console.log('New Issue Created: ', url);
       }
 
       rowNo++;
